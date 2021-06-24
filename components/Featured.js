03829258f1cf0eda883ccb98e47bd16b4141db66
ibaslogic <ibaskunle@gmail.com>
2021-06-24 07:41:57 +0100

    blog index page design updated

--- components/Featured.js
+++ components/Featured.js
@@ -4,9 +4,7 @@ import Image from "next/image";
 import useSWR from "swr";
 import fetcher from "../lib/fetcher";
 
-const BlogItem = (props) => {
-  const { title, description, slug, featured } = props;
-
+const FeaturedPost = ({ title, description, slug, featured }) => {
   const { data } = useSWR(`/api/views/${slug}`, fetcher);
   const views = data?.total;
 
@@ -21,7 +19,7 @@ const BlogItem = (props) => {
                   width={50}
                   height={50}
                   src={featured}
-                  className="rounded-full bg-gray-400"
+                  className="rounded-full bg-gray-200"
                   alt={title}
                 />
               ) : (
@@ -32,13 +30,11 @@ const BlogItem = (props) => {
             <div className="flex flex-wrap items-center justify-between text-gray-900 sm:col-span-2 sm:col-end-4">
               <h2 className="mr-2 font-medium text-lg md:text-xl">{title}</h2>
               <span className="text-sm block text-gray-500">
-                {`${
-                  views ? new Number(views).toLocaleString() : "–––"
-                } views`}
-              </span> 
+                {`${views ? new Number(views).toLocaleString() : "–––"} views`}
+              </span>
             </div>
             <div className="mt-3 col-start-1 col-end-4 sm:col-start-2 sm:col-end-4">
-              <p className=" text-gray-600 ">{description}</p>                
+              <p className=" text-gray-600 ">{description}</p>
             </div>
           </div>
         </a>
@@ -47,4 +43,4 @@ const BlogItem = (props) => {
   );
 };
 
-export default BlogItem;
+export default FeaturedPost;
--- pages/blog.js
+++ pages/blog.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Layout from "../components/Layout/Layout";
 import { getSortedPostData } from "../lib/mdx";
 import BlogItem from "../components/BlogItem";
-import Featured from "../components/Featured";
+import FeaturedPost from "../components/Featured";
 import Heading from "../components/Heading";
 import PageHeading from "../components/PageHeading";
 
@@ -56,20 +56,23 @@ const BlogPage = ({ postsData }) => {
           <section>
             <Heading title="Featured Posts" />
             <div>
-              <Featured
+              <FeaturedPost
                 title="React Tutorial From Scratch: A Step-by-Step Guide (2021)"
                 description="In this React tutorial for beginners, you will learn every step by step to build a React project. Besides, you will be able to deploy on the web."
                 slug="react-tutorial-for-beginners"
+                featured="/images/react-tutorial-for-beginners/react-tutorial-for-beginners.png"
               />
-              <Featured
+              <FeaturedPost
                 title="Working with React Form and Handling Event"
                 description="Form handling in React works a bit different compared to the regular HTML form. In this step-by-step guide, you will learn this in a simple way."
                 slug="react-form-handling"
+                featured="/images/react-form-handling/react-form-handling.png"
               />
-              <Featured
+              <FeaturedPost
                 title="How to implement Hamburger Menu in React"
                 description="In this part of the series, you’ll learn how to implement the functionality that allows you to toggle sidebar navigation."
                 slug="how-to-add-hamburger-menu-in-react"
+                featured="/images/how-to-add-hamburger-menu-in-react/how-to-add-hamburger-menu-in-react.png"
               />
             </div>
           </section>
