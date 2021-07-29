05077b4f44ee4c03a6c65a9fc353ba0c4ddfc271
ibaslogic <ibaskunle@gmail.com>
2021-07-29 23:36:48 +0100

    testing real time likes firebase

--- components/LikeCount.js
+++ components/LikeCount.js
@@ -1,56 +1,88 @@
 import { useState, useEffect } from "react";
 
-import firebase from "../lib/db";
+import useSWR from "swr";
+
+import fetcher from "../lib/fetcher";
+
+// import { firebase } from "../lib/db";
 import { BiLike } from "react-icons/bi";
 
 const LikeCount = ({ id }) => {
-  const [likes, setLikes] = useState("");
-  const [liked, setLiked] = useState(false);
-  const [disabled, setDisabled] = useState(false);
+  const { data } = useSWR(
+    `/api/likes/increment-likes?id=${encodeURIComponent(id)}`,
+    fetcher
+  );
+  const likes = new Number(data?.total);
 
-  useEffect(() => {
-    const onLikes = (newLikes) => setLikes(newLikes.val());
+  // useEffect(() => {
+  //   const registerLike = () =>
+  //     fetch(`/api/likes/increment-likes?id=${encodeURIComponent(id)}`, {
+  //       method: "POST",
+  //     });
 
-    const fetchData = async () => {
-      firebase.database().ref("likes").child(id).on("value", onLikes);
-    };
+  //   registerLike();
+  // }, [id]);
 
-    fetchData();
+  const handleLikes = async (e) => {
+    e.preventDefault();
 
-    return () => {
-      firebase.database().ref("likes").child(id).off("value", onLikes);
-    };
-  }, [id]);
+    const registerLike = () =>
+      fetch(`/api/likes/increment-likes?id=${encodeURIComponent(id)}`, {
+        method: "POST",
+      });
 
-  const like = async (e) => {
-    if (!liked) {
-      e.preventDefault();
-
-      const registerLike = () =>
-        fetch(`/api/likes/incrementLikes?id=${encodeURIComponent(id)}`);
-
-      registerLike();
-      setLiked(true);
-      setDisabled(true);
-    } else {
-      setDisabled(true);
-    }
+    registerLike();
   };
 
-  const btnDisabled = {
-    color: "#000",
-    cursor: "none",
-    opacity: "0.2",
-  };
+  // const [likes, setLikes] = useState(0);
+  // const [liked, setLiked] = useState(false);
+  // const [disabled, setDisabled] = useState(false);
+
+  // useEffect(() => {
+  //   const onLikes = (newLikes) => setLikes(newLikes.val());
+
+  //   const fetchData = async () => {
+  //     firebase.database().ref("likes").child(id).on("value", onLikes);
+  //   };
+
+  //   fetchData();
+
+  //   return () => {
+  //     firebase.database().ref("likes").child(id).off("value", onLikes);
+  //   };
+  // }, [id]);
+
+  // const like = async (e) => {
+  //   if (!liked) {
+  //     e.preventDefault();
+
+  //     const registerLike = () =>
+  //       fetch(`/api/likes/increment-likes?id=${encodeURIComponent(id)}`, {
+  //         method: "POST",
+  //       });
+
+  //     registerLike();
+  //     setLiked(true);
+  //     setDisabled(true);
+  //   } else {
+  //     setDisabled(true);
+  //   }
+  // };
+
+  // const btnDisabled = {
+  //   color: "#000",
+  //   cursor: "none",
+  //   opacity: "0.2",
+  // };
 
   return (
     <div className="lg:cust_line mt-8">
       <button
         className="grid grid-cols-2 gap-x-2 items-center text-lg focus:outline-none"
-        onClick={like}
-        style={disabled ? btnDisabled : null}
+        onClick={handleLikes}
+        // style={disabled ? btnDisabled : null}
       >
-        <BiLike /> {likes ? likes.toLocaleString() : "---"}
+        <BiLike /> {likes > 0 ? likes.toLocaleString() : "---"}
       </button>
     </div>
   );
